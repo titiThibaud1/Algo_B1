8b8b5261f00cbb4cdeeb36f38c3c45b54c302440
ThibaudS <thibaud.sellier@ynov.com>
2025-09-22 12:05:13 +0200

    Mise en code du pseudo code avec les infos accessoires et mise en page

--- Exo_pseudocode/main.go
+++ Exo_pseudocode/main.go
@@ -1,7 +1,31 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var Salle int
+	fmt.Println("Numéro de la salle de l'exam :")
+	fmt.Scan(&Salle)
+	fmt.Println("L'élève a obtenu un : ", QualiteNote(), ", à l'exam en salle", Salle)
+
+}
+
+func QualiteNote() string {
+	var Note float32
+	fmt.Println("Note de l'élève :")
+	fmt.Scan(&Note)
+	if Note < 0 || Note > 20 {
+		fmt.Println("\033[034mNote invalide\033[0m")
+		QualiteNote()
+	}
+	if Note < 7 {
+		return "\033[031mEchec\033[0m"
+	}
+	if Note > 13 {
+		return "\033[032mExcellent\033[0m"
+	} else {
+		return "\033[033mPassable\033[0m"
+	}
 }
 
 /*
