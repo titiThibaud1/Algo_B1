eee652485f480f670a5d43faac33e60eca4e506c
ThibaudS <thibaud.sellier@ynov.com>
2025-09-22 11:44:11 +0200

    Réalisation du pseudo code

--- Exo_pseudocode/main.go
+++ Exo_pseudocode/main.go
@@ -1,1 +1,26 @@
 package main
+
+func main() {
+
+}
+
+/*
+Exo 6
+Note : passable, echec, excellent en float
+Forcement entre 0 et 20 sinon erreur
+Salle de classe
+
+Pseudo code :
+input note
+(input classe)
+Si note < 0 ou > 20
+retourner une erreur
+Si note < 7
+	Note echec
+Si note > 13
+	Note Excellente
+Sinon
+	Note passable
+
+Afficher le numero de la salle + description
+*/
