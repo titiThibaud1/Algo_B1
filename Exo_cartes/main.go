4c43c3de10343c27069cda4e235e1a488e99a96f
ThibaudS <thibaud.sellier@ynov.com>
2025-09-23 11:21:20 +0200

    Nb de carte augmenté

---
+++ Exo_cartes/main.go
@@ -0,0 +1,31 @@
+package main
+
+func main() {
+
+}
+
+/*
+Pseudo code :
+
+func piocher
+Liste = [8]int{}
+for Liste[nb] = random de 0 à 16 exclue
+
+func vérif (Liste []int)
+for Liste[i] jusqu'à i == 8
+	si Liste[i]== 7
+	print "trouvé"
+
+*/
+
+/*
+Nombre de point du code pour 16 cartes
+2 + 1
++ (3 + 1) *15
+= 63
+
+Nombre de point du code pour 32 cartes
+2 + 1
++ (3 + 1) *31
+= 127
+*/
