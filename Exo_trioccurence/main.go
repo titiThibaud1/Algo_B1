6972a2eda7eb944a3a76496815893b8e157ac1d0
ThibaudS <thibaud.sellier@ynov.com>
2025-09-25 11:36:30 +0200

    Mise a jour pour test et pseudocode

--- Exo_trioccurence/main.go
+++ Exo_trioccurence/main.go
@@ -7,46 +7,46 @@ import (
 )
 
 func main() {
-	var ListeEntrée = []int{}          //Initialisation de la Liste donnée
-	for nb := 0; nb <= 5000000; nb++ { //Génère un tableau de 10 valeur aléatoire entre 0 et 5
+	var ListeEntrée = []int{}
+	for nb := 0; nb <= 5000000; nb++ {
 		intrdm := rand.Intn(15)
 		ListeEntrée = append(ListeEntrée, intrdm)
 	}
-	for random := len(ListeEntrée) - 1; random > 0; random-- { //Mélange aléatoirement la liste
+	for random := len(ListeEntrée) - 1; random > 0; random-- {
 		j := rand.Intn(random + 1)
 		ListeEntrée[random], ListeEntrée[j] = ListeEntrée[j], ListeEntrée[random]
 	}
 
-	start := time.Now() //Début du timer
+	start := time.Now()
 
-	ValeurMax := MaxValue(ListeEntrée)               //Appel de la fonction MaxValue qui sélectionne la valeur max du tableau
-	ListeSortie := Occurence(ListeEntrée, ValeurMax) //Appel de la fonction Tri qui tri le tableau
-	fmt.Println(ListeSortie)                         //Affichage de ListeSortie
+	ValeurMax := MaxValue(ListeEntrée)
+	ListeSortie := Occurence(ListeEntrée, ValeurMax)
+	fmt.Println(ListeSortie)
 
-	fin := time.Since(start) //Fin du timer
-	fmt.Println(fin)         //Affichage du temps de traitement
+	fin := time.Since(start)
+	fmt.Println(fin)
 }
 
-func MaxValue(ListeEntrée []int) int { //Fonction MaxValue pour déterminer le nombre de fois que la 1ère boucle de Tri va devoir boucler
-	var ValeurMax int                       //Initialisation de ValeurMax
-	for i := 0; i < len(ListeEntrée); i++ { //Boucle qui va parcourir toute la liste
-		if ValeurMax < ListeEntrée[i] { // Condition qui sélectione le plus grand nombre possible
+func MaxValue(ListeEntrée []int) int {
+	var ValeurMax int
+	for i := 0; i < len(ListeEntrée); i++ {
+		if ValeurMax < ListeEntrée[i] {
 			ValeurMax = ListeEntrée[i]
 		}
 	}
-	return ValeurMax //Retour du nombre maximum
+	return ValeurMax
 }
 
-func Occurence(ListeEntrée []int, ValeurMax int) []int { //Fonction Tri avec un tableau de 8 valeur en entrée et un tableau de taille x en sortie
-	var ListeSortie []int                                              //Initialisation de ValeurMax, tableau qui affichera le résultat
-	for CounterValue := 0; CounterValue <= ValeurMax; CounterValue++ { //Boucle qui va parcourir chaque nombre jusqu'à la valeur max
-		var ConteurNb int                                                        //Initialisation de CounterNb qui se fera écraser à chaque lecture de la boucle prédécente
-		for CounterListe := 0; CounterListe < len(ListeEntrée); CounterListe++ { //Boucle qui va parcourire toute ma Liste
-			if ListeEntrée[CounterListe] == CounterValue { //Condition que va incrémenter CounterNb pour chaque nombre trouvé égale à CounterValue (de la boucle précédente)
+func Occurence(ListeEntrée []int, ValeurMax int) []int {
+	var ListeSortie []int
+	for CounterValue := 0; CounterValue <= ValeurMax; CounterValue++ {
+		var ConteurNb int
+		for CounterListe := 0; CounterListe < len(ListeEntrée); CounterListe++ {
+			if ListeEntrée[CounterListe] == CounterValue {
 				ConteurNb++
 			}
 		}
-		ListeSortie = append(ListeSortie, ConteurNb) //Ajout à un nouveau tableau, les valeurs sortie de CounterNb
+		ListeSortie = append(ListeSortie, ConteurNb)
 	}
-	return ListeSortie //retourne le la liste du résultat
+	return ListeSortie
 }
